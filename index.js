b00661c82a8f972546e2d4ed17f28e4440e730a9
Jessica Lin <jessicaa.lin@gmail.com>
2019-06-09 15:24:40 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+let wakeDog = function(dogName, dogBreed){
+    let str = `Wake ${dogName} the ${dogBreed}`;
+    console.log(str);
+    return str;
+};
+
+let leashDog = function(dogName, dogBreed) {
+    let str = `Leash ${dogName} the ${dogBreed}`;
+    console.log(str);
+    return str;
+};
+
+let walkToPark = function(dogName, dogBreed) {
+    let str = `Walk to the park with ${dogName} the ${dogBreed}`;
+    console.log(str);
+    return str;
+};
+
+let throwFrisbee = function(dogName, dogBreed) {
+    let str = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+    console.log(str);
+    return str;
+};
+
+let walkHome = function(dogName, dogBreed) {
+    let str = `Walk home with ${dogName} the ${dogBreed}`;
+    console.log(str);
+    return str;
+};
+
+let unleashDog = function(dogName, dogBreed) {
+    let str = `Unleash ${dogName} the ${dogBreed}`;
+    console.log(str);
+    return str;
+};
+
+// let routine = [wakeDog(dogName, dogBreed), leashDog(dogName, dogBreed), walkToPark(dogName, dogBreed), throwFrisbee(dogName, dogBreed), walkHome(dogName, dogBreed), unleashDog(dogName, dogBreed)]
+
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+let exerciseDog = function (dogName, dogBreed) {
+    // for(let i=0, i<routine.length, i++){
+    //     return routine[i](dogName, dogBreed)
+    // }
+    let result = routine.map(func => func(dogName, dogBreed));
+    return result;
+}
